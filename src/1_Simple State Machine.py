530fe29d515a07566015be545c3d74ffb9470aae
Hsin1987 <ubuntu4bus@gmail.com>
2018-05-24 17:17:29 +0800

    Initial commit

---
+++ src/1_Simple State Machine.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+"""
+Modify the example from smach Learning by Example :
+* Link : http://wiki.ros.org/smach/Tutorials)
+And add the SMACH_Viewer
+* Reference: http://wiki.ros.org/smach/Tutorials/Smach%20Viewer
+"""
+import rospy
+import smach
+import smach_ros
+
+
+# define state Foo
+class Foo(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['outcome1', 'outcome2'])
+        self.counter = 0
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state FOO')
+        if self.counter < 10:
+            self.counter += 1
+            rospy.sleep(5.0)
+            return 'outcome1'
+        else:
+            rospy.sleep(5.0)
+            return 'outcome2'
+
+# define state Bar
+class Bar(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['outcome1'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state BAR')
+        rospy.sleep(5.0)
+        return 'outcome1'
+
+
+def main():
+    rospy.init_node('smach_example_state_machine')
+
+    # Create a SMACH state machine
+    sm = smach.StateMachine(outcomes=['outcome4'])
+
+    # Create and start the introspection server
+    sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
+    sis.start()
+
+    # Open the container
+    with sm:
+        # Add states to the container
+        smach.StateMachine.add('FOO', Foo(),
+                               transitions={'outcome1': 'BAR', 'outcome2': 'outcome4'})
+        smach.StateMachine.add('BAR', Bar(),
+                               transitions={'outcome1': 'FOO'})
+
+    # Execute SMACH plan
+    outcome = sm.execute()
+    sis.stop()
+
+if __name__ == '__main__':
+    main()
---
+++ src/README.md
@@ -0,0 +1,15 @@
+SMACH Tutorial
+==============
+```bash
+sudo apt-get install ros-kinetic-smach-viewer
+```
+
+### My Environment
+* OS : ubuntu 16.04
+* ROS: Kinetic Verion
+
+### Reference:
+* [SMACH Tutorial](http://wiki.ros.org/smach/Tutorials)
+* [SMACH Viewer Tutorial](http://wiki.ros.org/action/fullsearch/smach/Tutorials/Smach%20Viewer?action=fullsearch&context=180&value=linkto%3A%22smach%2FTutorials%2FSmach+Viewer%22)
+* [ROS探索总结（三十七）——有限状态机smach (1)](http://www.guyuehome.com/1069)
+* [ROS探索总结（三十八）——有限状态机smach (2)](http://www.guyuehome.com/1086)
